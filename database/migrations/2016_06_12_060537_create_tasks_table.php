cb055d66be4feb0f69f60bce9f9f12e185f48291
jeremykenedy <jeremy@grayboxpdx.com>
2016-06-11 23:06:35 -0700

    create database schema migrations

---
+++ database/migrations/2016_06_12_060537_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->unsigned()->default(0);
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->string('name')->default('');
+            $table->string('slug')->default('');
+            $table->boolean('completed')->default(false);
+            $table->text('description')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
